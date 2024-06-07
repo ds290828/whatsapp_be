8793acf9bd320bdbd9c54e3a8b878049d2705178
ds290828 <ds290828@gmail.com>
2024-06-07 09:11:42 +0530

    custom-logger

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import app from './app.js'
+import logger from './configs/logger.config.js'
 
 
 // env variables
@@ -7,6 +8,6 @@ console.log(process.env.NODE_ENV);
 
 app.listen(PORT,()=>{
    // console.log("Server is running at",PORT);
-    console.log(`Server is running on port ${PORT}`);
+    logger.info(`Server is running on port ${PORT}`);
 });
 
