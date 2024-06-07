6ae07f49d7aa521dd187057ad29d87ad9d474cb5
ds290828 <ds290828@gmail.com>
2024-06-07 08:38:10 +0530

    setup cross-env, adding middlewares

--- src/app.js
+++ src/app.js
@@ -1,9 +1,56 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import mongoSanitize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import fileUpload from "express-fileupload";
+import cors from "cors";
+
+//Dotenv config for accessing the variable that we define .dotenv file.
+dotenv.config();
 
 // create express app
-const app=express();
+const app = express();
 
-app.get('/', (req,res)=>{
+//morgan
+if (process.env.NODE_ENV != "production") {
+   app.use(morgan('dev'));
+}
+
+//helmet
+app.use(helmet());
+
+//parse json request url
+app.use(express.json());
+
+// parse json request body
+app.use(express.urlencoded({ extended: true }));
+
+//sanitize request data
+app.use(mongoSanitize())
+
+//enable cookie parser
+app.use(cookieParser())
+
+//gzip compression
+app.use(compression())
+
+//file upload
+app.use(fileUpload({
+   useTempFiles: true,
+}))
+
+//cors
+app.use(cors());
+
+
+app.post('/test', (req, res) => {
+   console.log(req.body);
+})
+
+app.get('/', (req, res) => {
    res.send("Hello from server.");
 })
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-import dotenv from 'dotenv';
 import app from './app.js'
 
 
-//Dotenv config for accessing the variable that we define .dotenv file.
-dotenv.config();
-
 // env variables
 const PORT = process.env.PORT || 8000;
-
+console.log(process.env.NODE_ENV);
 
 app.listen(PORT,()=>{
    // console.log("Server is running at",PORT);
