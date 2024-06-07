73d4ef4cefc394f0fa94528e8dd0c09092ac4bd3
ds290828 <ds290828@gmail.com>
2024-06-07 07:38:11 +0530

    setup express nodemon dotenv

--- src/index.js
+++ src/index.js
@@ -11,6 +11,6 @@ const PORT = process.env.PORT || 8000;
 
 app.listen(PORT,()=>{
    // console.log("Server is running at",PORT);
-    console.log(`Server is running on port ${PORT}..`);
+    console.log(`Server is running on port ${PORT}`);
 });
 
